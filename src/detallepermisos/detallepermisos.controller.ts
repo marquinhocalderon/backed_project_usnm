92f744e8fef20a2a3b972d93f5c0f45d027773df
mcalderon <mcalderon>
2024-09-07 15:39:12 -0500

    se agrego tanto para json y formdata

--- src/detallepermisos/detallepermisos.controller.ts
+++ src/detallepermisos/detallepermisos.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UseInterceptors } from '@nestjs/common';
 import { DetallepermisosService } from './detallepermisos.service';
 import { CreateDetallepermisoDto } from './dto/create-detallepermiso.dto';
 import { UpdateDetallepermisoDto, UpdatePeticiones } from './dto/update-detallepermiso.dto';
@@ -6,6 +6,7 @@ import { AuthGuard } from '../auth/guards/auth.guard';
 import { Permission } from '../auth/decorators/permission.decorator';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { MainDto, UsuarioDto2 } from './dto/get-usuarios-permisos.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 
 
@@ -36,6 +37,7 @@ export class DetallepermisosController {
   @ApiTags('DetallePermisos')
   @ApiBody({ type: [UpdateDetallepermisoDto] })
   @Patch('modulospermisos/:id_usuario')
+  @UseInterceptors(FileInterceptor('')) // Interceptor para procesar form-data
   @Permission('update-detallepermisos')
   update(@Param('id_usuario') id: string, @Body() updateDetallepermisoDtos: UpdateDetallepermisoDto[]) {
     return this.detallepermisosService.update(+id, updateDetallepermisoDtos);
@@ -44,6 +46,7 @@ export class DetallepermisosController {
   @ApiTags('DetallePermisos')
   @ApiBody({ type: [UpdatePeticiones] })
   @Patch('submodulospermisos/:id_usuario')
+  @UseInterceptors(FileInterceptor('')) // Interceptor para procesar form-data
   @Permission('update-detallepermisos')
   updatePeticiones(@Param('id_usuario') id: string, @Body() updatePeticiones: UpdatePeticiones[]) {
     return this.detallepermisosService.updatePeticiones(+id, updatePeticiones);

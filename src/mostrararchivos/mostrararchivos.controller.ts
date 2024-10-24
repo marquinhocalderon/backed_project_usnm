54fd45c85efefb9636f0a5317666e00e7d87d5ec
mcalderon <mcalderon>
2024-10-23 23:04:04 -0500

    se agrego foto a facultad

--- src/mostrararchivos/mostrararchivos.controller.ts
+++ src/mostrararchivos/mostrararchivos.controller.ts
@@ -29,6 +29,27 @@ export class MostrararchivosController {
     }
   }
 
+  @Get('imagen_facultad/:url_imagen')
+  getImagen2(@Param('url_imagen') filename: string, @Res() res: Response) {
+
+    // Construye la ruta completa del archivo usando el directorio actual de trabajo
+    const filePath = path.join(__dirname, '..', '..', 'uploads', 'facultadesimagenes', filename);
+
+    // Verifica si el archivo existe
+    if (fs.existsSync(filePath)) {
+      // Envía el archivo al cliente
+      res.sendFile(filePath, (err) => {
+        if (err) {
+          console.error('Error al enviar el archivo:', err);
+          throw new HttpException('Error al enviar la imagen', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+      });
+    } else {
+      // Lanza una excepción si el archivo no se encuentra
+      throw new HttpException('Imagen no encontrada', HttpStatus.NOT_FOUND);
+    }
+  }
+
   @Get('txt/:filename')
   getTxtFile(@Param('filename') filename: string, @Res() res: Response) {
     console.log(filename);
